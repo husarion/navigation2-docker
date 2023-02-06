663f6e95d99458372fd2060a4c3f0e8f6c76bf5d
dominikn <dominik.nwk@gmail.com>
2023-02-06 23:41:54 +0100

    added healthchecks

--- healthcheck_localization.py
+++ healthcheck_localization.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 import rclpy
-from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 
 def callback(msg):
-    print(f"received map")
+    print(f"received amcl_pose")
     exit(0)
 
 rclpy.init(args=[])
-node = rclpy.create_node("listener_map")
-sub = node.create_subscription(OccupancyGrid, "/map", callback, 10)
+latching_qos = QoSProfile(depth=1,
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
+
+node = rclpy.create_node("amcl_pose_listener")
+sub = node.create_subscription(PoseWithCovarianceStamped, "/amcl_pose", callback, qos_profile=latching_qos)
 rclpy.spin_once(node)
-print(f"map timeout")
+print(f"amcl_pose timeout")
 exit(1)

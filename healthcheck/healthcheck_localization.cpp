8d741551d32a5cf93ea5b564f75d841cd6c4cd60
rafal-gorecki <rafal.gorecki@husarion.com>
2023-11-17 15:27:13 +0100

    formatting

--- healthcheck/healthcheck_localization.cpp
+++ healthcheck/healthcheck_localization.cpp
@@ -1,36 +1,36 @@
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "fstream"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 
 #define LOOP_PERIOD 2s
 
-// State 'healthy' is latched, because topic 'amcl_pose' is transient_local and send only when moving.
+// State 'healthy' is latched, because topic 'amcl_pose' is transient_local and
+// send only when moving.
 
-void write_health_status(const std::string& status)
-{
+void write_health_status(const std::string &status) {
   std::ofstream healthFile("/health_status.txt");
   healthFile << status;
 }
 
-void msg_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
-{
+void msg_callback(
+    const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) {
   std::cout << "Message received" << std::endl;
   write_health_status("healthy");
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("healthcheck_node");
   rclcpp::QoS qos(1);
   qos.transient_local();
-  auto sub = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/amcl_pose", qos, msg_callback);
+  auto sub =
+      node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+          "/amcl_pose", qos, msg_callback);
 
   write_health_status("unhealthy");
-  while (rclcpp::ok())
-  {
+  while (rclcpp::ok()) {
     rclcpp::spin_some(node);
     std::this_thread::sleep_for(LOOP_PERIOD);
   }
--- healthcheck/healthcheck_slam.cpp
+++ healthcheck/healthcheck_slam.cpp
@@ -1,8 +1,8 @@
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav2_msgs/srv/save_map.hpp"
-#include <fstream>  // Include <fstream> for file operations
-#include <thread>   // Include <thread> for sleep_for
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include <fstream>
+#include <thread>
 
 using namespace std::chrono_literals;
 
@@ -12,47 +12,44 @@ using namespace std::chrono_literals;
 #define SAVE_MAP_CONNECTION_TIMEOUT 2s
 #define SAVE_MAP_VALID_TIME 20s
 
-class HealthCheckNode : public rclcpp::Node
-{
+class HealthCheckNode : public rclcpp::Node {
 public:
-  HealthCheckNode() : Node("healthcheck_node")
-  {
+  HealthCheckNode() : Node("healthcheck_node") {
     rclcpp::QoS qos(1);
     qos.transient_local();
     map_subscriber_ = create_subscription<nav_msgs::msg::OccupancyGrid>(
-        "/map", qos, std::bind(&HealthCheckNode::msgCallback, this, std::placeholders::_1));
-    save_map_client_ = create_client<nav2_msgs::srv::SaveMap>("/map_saver/save_map");
+        "/map", qos,
+        std::bind(&HealthCheckNode::msgCallback, this, std::placeholders::_1));
+    save_map_client_ =
+        create_client<nav2_msgs::srv::SaveMap>("/map_saver/save_map");
   }
 
-  void healthyCheck()
-  {
+  void healthyCheck() {
     auto current_time = std::chrono::steady_clock::now();
-    std::chrono::duration<double> msg_elapsed_time = current_time - last_msg_time;
-    std::chrono::duration<double> saved_map_elapsed_time = current_time - last_saved_map_time;
+    std::chrono::duration<double> msg_elapsed_time =
+        current_time - last_msg_time;
+    std::chrono::duration<double> saved_map_elapsed_time =
+        current_time - last_saved_map_time;
     bool is_msg_valid = msg_elapsed_time.count() < MSG_VALID_TIME.count();
-    bool is_save_map_valid = saved_map_elapsed_time.count() < SAVE_MAP_VALID_TIME.count();
+    bool is_save_map_valid =
+        saved_map_elapsed_time.count() < SAVE_MAP_VALID_TIME.count();
 
-    if (is_msg_valid && is_save_map_valid)
-    {
+    if (is_msg_valid && is_save_map_valid) {
       RCLCPP_INFO(get_logger(), "Health check: healthy");
       writeHealthStatus("healthy");
-    }
-    else
-    {
+    } else {
       RCLCPP_WARN(get_logger(), "Health check: unhealthy");
       writeHealthStatus("unhealthy");
     }
   }
 
-  void saveMapPeriodically()
-  {
+  void saveMapPeriodically() {
     auto current_time = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_time = current_time - last_saved_map_time;
+    std::chrono::duration<double> elapsed_time =
+        current_time - last_saved_map_time;
 
-    if (elapsed_time.count() > SAVE_MAP_PERIOD.count())
-    {
-      if (save_map_client_->wait_for_service(SAVE_MAP_CONNECTION_TIMEOUT))
-      {
+    if (elapsed_time.count() > SAVE_MAP_PERIOD.count()) {
+      if (save_map_client_->wait_for_service(SAVE_MAP_CONNECTION_TIMEOUT)) {
         RCLCPP_DEBUG(get_logger(), "Service available");
         auto request = std::make_shared<nav2_msgs::srv::SaveMap::Request>();
         request->free_thresh = 0.15;
@@ -63,18 +60,15 @@ public:
 
         auto future = save_map_client_->async_send_request(request);
 
-        if (rclcpp::spin_until_future_complete(shared_from_this(), future, SAVE_MAP_CONNECTION_TIMEOUT) == rclcpp::FutureReturnCode::SUCCESS)
-        {
+        if (rclcpp::spin_until_future_complete(shared_from_this(), future,
+                                               SAVE_MAP_CONNECTION_TIMEOUT) ==
+            rclcpp::FutureReturnCode::SUCCESS) {
           RCLCPP_DEBUG(get_logger(), "Map saved");
           last_saved_map_time = std::chrono::steady_clock::now();
-        }
-        else
-        {
+        } else {
           RCLCPP_DEBUG(get_logger(), "Service response didn't arrived");
         }
-      }
-      else
-      {
+      } else {
         RCLCPP_DEBUG(get_logger(), "Service unavailable");
       }
     }
@@ -86,26 +80,22 @@ private:
   std::chrono::steady_clock::time_point last_msg_time;
   std::chrono::steady_clock::time_point last_saved_map_time;
 
-  void writeHealthStatus(const std::string &status)
-  {
+  void writeHealthStatus(const std::string &status) {
     std::ofstream healthFile("/health_status.txt");
     healthFile << status;
   }
 
-  void msgCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)
-  {
+  void msgCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
     RCLCPP_DEBUG(get_logger(), "Msg arrived");
     last_msg_time = std::chrono::steady_clock::now();
   }
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<HealthCheckNode>();
 
-  while (rclcpp::ok())
-  {
+  while (rclcpp::ok()) {
     rclcpp::spin_some(node);
     node->healthyCheck();
     node->saveMapPeriodically();
